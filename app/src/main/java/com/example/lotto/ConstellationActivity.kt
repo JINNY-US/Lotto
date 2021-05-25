2986ef34af560a364894937e968167ea6a353d0d
JINNY.US <kyungjin2215@gmail.com>
2021-05-25 09:48:00 +0900

    Intent

--- app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -3,13 +3,17 @@ package com.example.lotto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.cardview.widget.CardView
+import android.widget.Button
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
 
+        val btnGoResult = findViewById<Button>(R.id.btnGoName)
+        btnGoResult.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
 
     }
 }
